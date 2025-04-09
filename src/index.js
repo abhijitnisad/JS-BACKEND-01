b046b15c2751696c6f310f89ce8465f9999b3d95
abhijitnisad <abhijit.nisad99@gmail.com>
2025-04-09 12:19:51 +0530

    index.js  complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-export const DB_NAME = "videotube";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express()
+(async () => {
+    try {
+        mongoose.connect(`${process.env.MONGO_URI}`)
+        app.on("error", (error) => {
+            console.log("ERROR: " , error);
+            throw error
+        
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+            
+        })
+
+    } catch (error) {
+        console.error("Error: ", error);
+        throw err
+        
+    }
+})()
+    */
